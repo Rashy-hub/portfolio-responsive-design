e9f3a491baa290352893c54f38af7297d5fc6d5a
Rashy-hub <yagoubi.r@gmail.com>
2024-03-20 15:21:20 +0100

    refactor (script) : Refactored animation logic with reusable function

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
 console.log('Starting script.js')
-const skillElements = document.querySelectorAll(
-    '.skills__container__element:nth-child(n)'
-)
 
-skillElements.forEach((element, index) => {
-    const delay = 0.2 * index // Recalculer le délai en utilisant l'index
-    element.style.animationDelay = delay + 's'
+function animateNodeListOnIntersection(selector) {
+    const elements = document.querySelectorAll(selector)
 
-    // Déclencher l'animation à l'intersection
-    const observerA = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate')
-                observerA.unobserve(entry.target)
-            }
+    elements.forEach((element, index) => {
+        element.style.animationDelay = `${0.2 * index}s`
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('animate')
+                    observer.unobserve(entry.target)
+                }
+            })
         })
+
+        observer.observe(element)
     })
-    observerA.observe(element)
-})
+}
 
-const servicesElements = document.querySelectorAll(
-    '.services__card__bx:nth-child(n)'
-)
-
-servicesElements.forEach((element, index) => {
-    const delay = 0.2 * index // Recalculer le délai en utilisant l'index
-    element.style.animationDelay = delay + 's'
-
-    // Déclencher l'animation à l'intersection
-    const observerB = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate')
-                observerB.unobserve(entry.target)
-            }
-        })
-    })
-    observerB.observe(element)
-})
+animateNodeListOnIntersection('.skills__container__element:nth-child(n)')
+animateNodeListOnIntersection('.services__card__bx:nth-child(n)')
