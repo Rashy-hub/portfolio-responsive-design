26a4ff21bd26f75254917e9ebf347678608ae708
Rashy-hub <yagoubi.r@gmail.com>
2024-03-21 21:19:25 +0100

    feat (script) : Add new function to toggle active nav elements

--- script.js
+++ script.js
@@ -18,5 +18,28 @@ function animateNodeListOnIntersection(selector) {
     })
 }
 
+function addEventListenersOnDOMelements(selector, event) {
+    const elements = document.querySelectorAll(selector)
+    elements.forEach((element) => {
+        element.addEventListener(event, (e) => {
+            e.target.classList.toggle(className)
+        })
+    })
+}
+
+function toggleActiveNavElements() {
+    const navItems = document.querySelectorAll('.navbar__item')
+    navItems.forEach((item) => {
+        item.addEventListener('click', function () {
+            navItems.forEach((navItem) => {
+                console.log(navItem)
+                navItem.classList.remove('navbar__item--active')
+            })
+            this.classList.add('navbar__item--active')
+        })
+    })
+}
+
 animateNodeListOnIntersection('.skills__container__element:nth-child(n)')
 animateNodeListOnIntersection('.services__card__bx:nth-child(n)')
+toggleActiveNavElements()
